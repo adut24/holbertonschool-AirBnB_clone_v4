962cd973d217644103b1c2e202848a9e6c2b55bb
adut24 <dutertre.alexandre@laposte.net>
2022-09-23 12:12:41 +0200

    Push task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,56 @@
+$.when($.ready).then(function () {
+  const dictAmenitiesId = {};
+  $('[type="checkbox"]').click(function () {
+    const name = $(this).attr('data-name');
+    const id = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      dictAmenitiesId[id] = name;
+    } else {
+      delete dictAmenitiesId[id];
+    }
+    const listNameAmenities = [];
+    for (const [, value] of Object.entries(dictAmenitiesId)) {
+      listNameAmenities.push(value);
+    }
+    $('.amenities h4').text(listNameAmenities.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (typeof data === 'object' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    data: JSON.stringify({}),
+    headers: { 'Content-Type': 'application/json' },
+    dataType: 'json',
+    success: function (response) {
+      for (let i = 0; i < 2; i++) {
+        $('.places').append('<article></article>');
+        $('article:last').append('<div></div>');
+        $('article div').addClass('title_box');
+        $('.title_box:last').append('<h2>' + response[i].name + '</h2>');
+        $('.title_box:last').append('<div></div>');
+        $('.title_box:last div').addClass('price_by_night');
+        $('.price_by_night:last').append('$' + response[i].price_by_night);
+        $('article:last').append('<div></div>');
+        $('article:last div:last').addClass('information');
+        $('.information:last').append('<div></div>');
+        $('.information:last div').addClass('max_guest');
+        $('.max_guest:last').append(response[i].max_guest + ' Guest');
+        $('.information:last').append('<div></div>');
+        $('.information:last div:last').addClass('number_rooms');
+        $('.number_rooms:last').append(response[i].number_rooms + ' Bedroom');
+        $('.information:last').append('<div></div>');
+        $('.information:last div:last').addClass('number_bathrooms');
+        $('.number_bathrooms:last').append(response[i].number_bathrooms + ' Bathroom');
+        $('article:last').append('<div></div>');
+        $('article:last div:last').addClass('description');
+        $('.description').append(response[i].description);
+      }
+    }
+  });
+});
